12c39e7fd035210840e05336925d9bf08a28a940
ExGules <Respekt32113113@mail.ru>
2024-11-14 22:17:00 +0400

    Intial commit

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -7,7 +7,7 @@ export const ArticleParamsForm = () => {
 	return (
 		<>
 			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<aside className={styles.container_open}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
